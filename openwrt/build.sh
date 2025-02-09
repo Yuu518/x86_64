9fccdfbab853dee60e2334603d929f43a852c85b
Yuu <yuumi2836@gmail.com>
2025-02-09 09:23:37 +0800

    Custom Update

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -30,9 +30,9 @@ ip_info=`curl -sk https://ip.cooluc.com`;
 
 # script url
 if [ "$isCN" = "CN" ]; then
-    export mirror=https://init.cooluc.com
+    export mirror=https://raw.githubusercontent.com/Yuu518/x86_64/master
 else
-    export mirror=https://init2.cooluc.com
+    export mirror=https://raw.githubusercontent.com/Yuu518/x86_64/master
 fi
 
 # github actions - caddy server
--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-# 自定义脚本
+# add mihomo
+git clone https://$github/morytyann/openwrt-mihomo package/new/openwrt-mihomo
+rm -rf package/new/helloworld/luci-app-mihomo
+rm -rf package/new/helloworld/mihomo
+if curl -s "https://$mirror/openwrt/23-config-common" | grep -q "^CONFIG_PACKAGE_luci-app-mihomo=y"; then
+    mkdir -p files/etc/mihomo/run/ui
+    curl -Lso files/etc/mihomo/run/geoip.metadb https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.metadb
+    curl -Lso files/etc/mihomo/run/ASN.mmdb https://$github/xishang0128/geoip/releases/download/latest/GeoLite2-ASN.mmdb
+    curl -Lso metacubexd-gh-pages.tar.gz https://$github/MetaCubeX/metacubexd/archive/refs/heads/gh-pages.tar.gz
+    tar zxf metacubexd-gh-pages.tar.gz
+    rm metacubexd-gh-pages.tar.gz
+    mv metacubexd-gh-pages files/etc/mihomo/run/ui/metacubexd
+fi
+
+# add ddns-go
+git clone https://$github/sirpdboy/luci-app-ddns-go package/new/ddns-go
